5b50850a0d6e38bad73680d2d0ce5ab5dcd402a7
itasneem2004 <idrisitasneembsk@gmail.com>
2024-08-28 18:03:25 +0530

    task

---
+++ src/components/home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../layout/header.js';
+import Footer from '../layout/footer.js';
+
+const index = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default index
