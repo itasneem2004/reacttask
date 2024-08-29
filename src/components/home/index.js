6ac58acf4efd6d788eb68a7f107d713a43754b47
itasneem2004 <idrisitasneembsk@gmail.com>
2024-08-29 18:05:27 +0530

    incomplete

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,10 +1,39 @@
 import React from 'react'
 import Header from '../layout/header.js';
+import Logo from '../../assets/logo.png';
 import Footer from '../layout/footer.js';
+import Carousel from 'react-bootstrap/Carousel';
 
 const index = () => {
   return (
     <>
+      <Header />
+      <Carousel>
+        <Carousel.Item>
+          <img src={Logo} />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={Logo} />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={Logo} />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      <Footer />
     </>
   )
 }
--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -2,30 +2,47 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Logo from '../../assets/logo.png';
 
 
 const header = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
     <Container>
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={Logo}
+              width="60px"
+              height="60px"
+              className="d-inline-block align-top"
+              alt="creative-logo"
+            />
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Navbar.Brand href="#home">National Defence Academy</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
+          <Nav.Link href="#aboutus">About Us</Nav.Link>
+          <Nav.Link href="#facility">Facilities</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+          
+         
         </Nav>
       </Navbar.Collapse>
     </Container>
