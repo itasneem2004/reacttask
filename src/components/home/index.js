b1f1a904c3ed162a3c8168f253ebf11f24b9e0df
itasneem2004 <idrisitasneembsk@gmail.com>
2024-09-03 18:05:26 +0530

    updated1

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -7,6 +7,18 @@ import Logo3 from '../../assets/5.jpg';
 import Footer from '../layout/footer.js';
 import Carousel from 'react-bootstrap/Carousel';
 import Logo4 from '../../assets/6.jpg';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Img1 from '../../assets/Img.jpg';
+import Photo1 from '../../assets/photo1.jpg';
+import Photo2 from '../../assets/photo2.jpg';
+import Photo3 from '../../assets/photo3.jpg';
+import Photo4 from '../../assets/photo4.jpg';
+
+
+
+
 
 const index = () => {
   return (
@@ -44,8 +56,35 @@ const index = () => {
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
-      <p></p>
-      <img src={Logo4} height={350} width={350}/>
+      <Container>
+        <Row className="align-item-start">
+          <Col xs={12} md={6}>
+            <img src={Img1} height={350} width={350} />
+          </Col>
+          <Col xs={12} md={6} className="text-start">
+          <h1>WHAT IS NDA</h1>
+          <h2>(NATIONAL DEFENCE ACADEMY)</h2>
+          <p>The National Defence Academy (NDA) is a premier military academy in India that trains future leaders of the Indian Armed Forces.The National Defence Academy is a cornerstone institution for the Indian Armed Forces, providing rigorous training and education to future military leaders. Its comprehensive approach to training ensures that cadets are well-prepared for the challenges of military service and leadership.</p>
+          </Col>
+          </Row>
+      </Container>
+
+      <Container>
+        <Row>
+          <Col xs={12} md={3} className="mb-3">
+          <img src={Photo1} height={300} width={300}/>
+          </Col>
+          <Col xs={12} md={3} className="mb-3">
+          <img src={Photo2}height={300} width={300}/>
+          </Col>
+          <Col xs={12} md={3} className="mb-3">
+          <img src={Photo3}height={300} width={300}/>
+          </Col>
+          <Col xs={12} md={3} className="mb-3">
+          <img src={Photo4}height={300} width={300}/>
+          </Col>
+        </Row>
+      </Container>
       <Footer />
     </>
   )
